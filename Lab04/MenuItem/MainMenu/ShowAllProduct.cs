9eba5c6d645c6897405fead24953db9dd09df7b4
wildman9x <wildmanyt@gmail.com>
2023-06-07 06:29:50 +0700

    update

--- Lab04/MenuItem/MainMenu/ShowAllProduct.cs
+++ Lab04/MenuItem/MainMenu/ShowAllProduct.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ConsoleTables;
 using Lab04.Interface;
 using Lab04.Model;
 using Lab04.Singleton;
@@ -15,10 +16,16 @@ namespace Lab04.MenuItem.MainMenu
         public void Run()
         {
             Console.WriteLine("List of products:");
+            // foreach (var product in ProductDict.Instance.Products)
+            // {
+            //     Console.WriteLine(product);
+            // }
+            var table = new ConsoleTable("ID", "Name", "Price");
             foreach (var product in ProductDict.Instance.Products)
             {
-                Console.WriteLine(product);
+                table.AddRow(product.Key, product.Value.Name, product.Value.Price);
             }
+            table.Write();
         }
     }
 }
