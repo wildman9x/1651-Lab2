2113dda94e6ab045c7f85264c0b85b2f6f197624
wildman9x <wildmanyt@gmail.com>
2023-06-06 21:37:52 +0700

    update

--- Lab04/Program.cs
+++ Lab04/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Lab04.Interface;
 using Lab04.MenuItem;
@@ -11,10 +12,20 @@ class Program
     static void Main(string[] args)
     {
         var Context = new Context();
-        Dictionary<int, IMenuItem> menuItems = new Dictionary<int, IMenuItem>();
-        menuItems.Add(menuItems.Count, new MenuItem.MainMenu.Exit());
-        menuItems.Add(menuItems.Count, new MenuItem.MainMenu.ShowAllProduct());
-        menuItems.Add(menuItems.Count, new MenuItem.MainMenu.AddProduct());
+        // Create all class in the MenuItem.MainMenu namespace and put into the menuItems dictionary
+        List<IMenuItem> menuItemsList = Assembly.GetExecutingAssembly().GetTypes()
+            .Where(t => t.Namespace == "Lab04.MenuItem.MainMenu" && t.IsClass && !t.IsAbstract && typeof(IMenuItem).IsAssignableFrom(t))
+            .Select(t => Activator.CreateInstance(t) as IMenuItem).ToList();
+        Dictionary<int, IMenuItem> menuItems = new();
+        foreach (var menuItem in menuItemsList)
+        {
+            menuItems.Add(menuItems.Count, menuItem);
+        }
+        
+        // menuItems.Add(menuItems.Count, new MenuItem.MainMenu.Exit());
+        // menuItems.Add(menuItems.Count, new MenuItem.MainMenu.ShowAllProduct());
+        // menuItems.Add(menuItems.Count, new MenuItem.MainMenu.AddProduct());
+        
 
         while (true)
         {
