0ccc9c64d587937dadeb8ca6e613ba39c9483fa4
Anh Trinh <wildmanyt@gmail.com>
2023-06-10 16:51:37 +0700

    update

---
+++ Lab05/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Lab05;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
